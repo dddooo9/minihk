cf58690be733732585dc67440dcb8a99622097d6
dddooo9 <dlaehdus99@gmail.com>
2020-08-16 21:57:50 +0900

    admin

--- posts/admin.py
+++ posts/admin.py
@@ -2,4 +2,21 @@ from django.contrib import admin
 from .models import Product, Review
 # Register your models here.
 admin.site.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display=(
+        'id',
+        'title',
+        'view_count',
+        'created_at',
+    )
+    list_filter=(
+        'permission',
+    )
+    search_fields=(
+        'id',
+        'title'
+    )
+
+
+
 admin.site.register(Review)
